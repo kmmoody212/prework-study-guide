b155f97a529aab920ea7547a9e44ddb2a0bbd3df
Katrina Moody <137461461+kmmoody212@users.noreply.github.com>
2024-09-15 17:43:29 -0400

    Added JavaScipt page and linked to HTML doc

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+const topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+for (let i = 0; i < topics.length; i++) {
+  console.log(topics[i]);
+}
+/*
+if (topic === "HTML") {
+  console.log("Let's study HTML!");
+} else if (topic === "CSS") {
+  console.log("Let's study CSS!");
+} else if (topic === "Git") {
+  console.log("Let's study Git!");
+} else if (topic === "JavaScript") {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log("Please try again!");
+}
+
+const shapes = ["triangle", "square", "pentagon", "circle"];
+console.log(shapes[0]);
+console.log(shapes[1]);
+console.log(shapes[2]);
+console.log(shapes[3]);
+
+for (let x = 0; x < shapes.length; x++) {
+  console.log(shapes[x]);
+}
+*/
